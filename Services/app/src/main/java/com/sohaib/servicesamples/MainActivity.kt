d7324622a1b9da9d3e750f5195e50a87cac362ee
epegasus <sohaibahmed.hypersoft@gmail.com>
2026-04-01 16:35:33 +0500

    Initial Commit

---
+++ Services/app/src/main/java/com/sohaib/servicesamples/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.sohaib.servicesamples
+
+import android.app.ActivityManager
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.sohaib.servicesamples.databinding.ActivityMainBinding
+import com.sohaib.servicesamples.services.background.MyBackgroundService
+import com.sohaib.servicesamples.services.bound.MyBoundService
+import com.sohaib.servicesamples.services.foreground.MyForegroundService
+import com.sohaib.servicesamples.services.lifecycle.MyServiceLifeCycle
+
+const val TAG = "MyTag"
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    // think you are going to make instagram & Music Player
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        //initServiceLifeCycle()
+        //initBackgroundService()
+        //initForegroundService()
+        //initBoundService()
+        initBoundService()
+
+        binding.btnBind.setOnClickListener { onBindClick() }
+        binding.mbResult.setOnClickListener { onResultClick() }
+    }
+
+    private fun initServiceLifeCycle() {
+        val intent = Intent(this, MyServiceLifeCycle::class.java)
+        startService(intent)
+    }
+
+    private fun initBackgroundService() {
+        val intent = Intent(this, MyBackgroundService::class.java)
+        startService(intent)
+        //startService(intent)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startService(intent)
+        }, 3000)
+    }
+
+    private fun initForegroundService() {
+        val intent = Intent(this, MyForegroundService::class.java)
+        ContextCompat.startForegroundService(this, intent)
+    }
+
+    fun isServiceRunning(serviceClassName: String?): Boolean {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        for (runningServiceInfo in activityManager.getRunningServices(Int.MAX_VALUE)) {
+            if (runningServiceInfo.service.className == serviceClassName) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    /* --------------------------------------- Bound --------------------------------------- */
+
+    private var serviceConnection: ServiceConnection? = null
+    private var myBoundService: MyBoundService? = null
+    private val boundIntent by lazy { Intent(this, MyBoundService::class.java) }
+
+    private fun initBoundService() {
+        ContextCompat.startForegroundService(this, boundIntent)
+    }
+
+    private fun onBindClick() {
+        if (serviceConnection != null) {
+            return
+        }
+
+        serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                binding.tvStatus.text = "Connection Established"
+                myBoundService = (service as MyBoundService.LocalBinder).getService()
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                binding.tvStatus.text = "Connection Disconnected"
+            }
+        }.also {
+            bindService(boundIntent, it, BIND_AUTO_CREATE)
+        }
+    }
+
+    private fun onResultClick() {
+        val result = "${myBoundService?.getCounter() ?: -1}"
+        binding.tvStatus.text = result
+        if (myBoundService?.getCounter() == 101) {
+            serviceConnection?.let { unbindService(it) }
+        }
+    }
+}
---
+++ Services/app/src/main/java/com/sohaib/servicesamples/services/bound/MyBoundService.kt
@@ -0,0 +1,130 @@
+package com.sohaib.servicesamples.services.bound
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.pm.ServiceInfo
+import android.content.Intent
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.sohaib.servicesamples.MainActivity
+import com.sohaib.servicesamples.R
+import com.sohaib.servicesamples.TAG
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+@Suppress("DEPRECATION")
+class MyBoundService : Service() {
+
+    private lateinit var notificationManager: NotificationManager
+
+    /**
+     * Init all resources
+     */
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate: called")
+        notificationManager = getSystemService(NotificationManager::class.java)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+
+        val id = "em_id"
+        val name = "Soneya"
+        val description = "Nawaaa Ayaa?"
+        val channel = NotificationChannel(id, name, NotificationManager.IMPORTANCE_LOW).apply {
+            this.description = description
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        val builder = createNotification()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(
+                100,
+                builder.build(),
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+            )
+        } else {
+            startForeground(100, builder.build())
+        }
+
+        startWorking()
+
+        return START_STICKY
+    }
+
+    private fun createNotification(): NotificationCompat.Builder {
+        // If the notification supports a direct reply action, use PendingIntent.FLAG_MUTABLE instead.
+        val pendingIntent: PendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
+            PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        }
+
+        val channelId = "em_id"
+        val bitTextStyle = NotificationCompat.BigTextStyle().bigText("Much longer text that cannot fit one line...")
+
+        return NotificationCompat
+            .Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_svg_android)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setStyle(bitTextStyle)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+    }
+
+
+    /* -------------------------------------- Bound Logic -------------------------------------- */
+
+    private var counter = 0
+    private val binder = LocalBinder()
+
+    private fun startWorking() {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (counter <= 100) {
+                Log.d(TAG, "startWorking: $counter")
+                counter++
+                delay(1000)
+                startWorking()
+            } else {
+                stopForeground(STOP_FOREGROUND_REMOVE)
+                stopSelf()
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        Log.d(TAG, "onBind: called")
+        return binder
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+        Log.d(TAG, "onRebind: called")
+    }
+
+    fun getCounter(): Int {
+        return counter
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
+    }
+
+    inner class LocalBinder : Binder() {
+        fun getService(): MyBoundService = this@MyBoundService
+    }
+}
